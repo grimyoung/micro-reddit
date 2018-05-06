1c3184ce9f0243361b6f4e159cc8b0f0b6802798
grimyoung <samhyukim@gmail.com>
2018-05-06 15:10:00 -0700

    create comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 50 },
             uniqueness: {case_sensitive: true}
   has_many :posts
+  has_many :comments
 end
